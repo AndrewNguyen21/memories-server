6de41da7bc8878971d9f33a8d8898b68de363bfa
Andrew Nguyen <andrewnguyen@andrews-mbp.mynetworksettings.com>
2023-01-11 16:40:31 -0500

    changed back end for redux changes

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,37 @@
 import mongoose from 'mongoose';
 import PostMessage from '../models/postMessage.js';
 
-export const getPosts = async (req, res) => {
-  try {
-    const postMessages = await PostMessage.find();
+export const getPost = async (req, res) => {
+  const { id } = req.params;
 
-    res.status(200).json(postMessages);
+  try {
+    const post = await PostMessage.findById(id);
+
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+}
+
+export const getPosts = async (req, res) => {
+  const { page } = req.query;
+
+  try {
+    const LIMIT = 8;
+    const startIndex = (Number(page) - 1) * LIMIT;
+    const total = await PostMessage.countDocuments({});
+    const posts = await PostMessage.find()
+      .sort({ _id: -1 })
+      .limit(LIMIT)
+      .skip(startIndex);
+
+    res
+      .status(200)
+      .json({
+        data: posts,
+        currentPage: Number(page),
+        numberOfPages: Math.ceil(total / LIMIT),
+      });
   } catch (error) {
     res.status(404).json({ message: error });
   }
@@ -14,7 +40,11 @@ export const getPosts = async (req, res) => {
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage({...post, creator: req.userId, createdAt: new Date().toISOString()});
+  const newPost = new PostMessage({
+    ...post,
+    creator: req.userId,
+    createdAt: new Date().toISOString(),
+  });
 
   try {
     await newPost.save();
@@ -33,7 +63,11 @@ export const updatePost = async (req, res) => {
     return res.status(404).send('No post with that id');
   }
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id },
+    { new: true },
+  );
 
   res.json(updatedPost);
 };
@@ -66,13 +100,31 @@ export const likePost = async (req, res) => {
 
   const index = post.likes.findIndex((id) => id === String(req.userId));
 
-  if(index === -1) {
+  if (index === -1) {
     post.likes.push(req.userId);
   } else {
     post.likes = post.likes.filter((id) => id !== String(req.userId));
   }
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
+  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
+    new: true,
+  });
 
   res.json(updatedPost);
+};
+
+export const searchPosts = async (req, res) => {
+  const { searchQuery, tags } = req.query;
+  //console.log(req.query);
+  try {
+    const title = new RegExp(searchQuery, 'i');
+    //console.log(tags)
+    const posts = await PostMessage.find({
+      $or: [{ title }, { tags: { $in: tags.split(',') } }],
+    });
+
+    res.json({ data: posts });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
 };
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use('/posts', postRoutes);
 app.use('/users', userRoutes);
 
 //const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 mongoose
   .connect(process.env.CONNECTION_URL, { useNewURLParser: true, useUnifiedTopology: true })
