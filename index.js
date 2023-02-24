bec51b1d636a07f6f9a6296e5d0baffd5f43a629
Andrew Nguyen <andrewnguyen@andrews-mbp.mynetworksettings.com>
2023-02-24 18:41:59 -0500

    removed uneeded var

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ import userRoutes from './routes/user.js';
 const app = express();
 dotenv.config();
 
-var cors = require('cors')
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
